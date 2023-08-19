84311d7d3ccce791ca3be6f99570de9e68cfa9fa
CasterBasile <basilecastrese@gmail.com>
2023-08-19 23:56:41 +0200

    2.1.2

--- src/js/feed.js
+++ src/js/feed.js
@@ -119,4 +119,27 @@ fetch(url)
   // Aggiungi un event listener per chiudere l'elemento modale quando viene cliccato
 imageModal.addEventListener('click', closeImageModal);
 
-  
+const images = [
+  '/src/images/eremo.jpg',
+  '/src/images/foragnano.jpg',
+  '/src/images/gallery/gallery.jpg',
+  '/src/images/gallery/gallery2.jpg',
+  '/src/images/gallery/gallery3.jpg',
+  '/src/images/gallery/gallery4.jpg',
+  '/src/images/gallery/gallery5.jpg',
+];
+
+let currentIndex = 0;
+const linkTiles = document.querySelectorAll('.link-tile');
+
+function changeBackgroundImage() {
+  linkTiles.forEach((tile, index) => {
+      tile.style.backgroundImage = `url('${images[(currentIndex + index) % images.length]}')`;
+  });
+
+  currentIndex = (currentIndex + 1) % images.length;
+  setTimeout(changeBackgroundImage, 2800); // Cambia immagine ogni 1.5 secondi
+}
+
+changeBackgroundImage();
+
