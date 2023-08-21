d12cee36aa5574ccce338b04549a9b7e90a8e240
CasterBasile <basilecastrese@gmail.com>
2023-08-21 23:23:16 +0200

    3.3

--- src/js/events.js
+++ src/js/events.js
@@ -1,28 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const eventModal = document.getElementById("eventModal");
     const closeModal = document.querySelector(".close-modal");
-    const openModalButtons = document.querySelectorAll(".banner-link");
-    const saveToCalendarButton = document.querySelector(".save-to-calendar-button");
+    const openModalButtons = document.querySelectorAll(".banner-link.open-modal");
 
     openModalButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
+        button.addEventListener("click", function() {
             eventModal.style.display = "block";
-
-            const title = button.getAttribute("data-title");
-            const date = button.getAttribute("data-date");
-            const time = button.getAttribute("data-time");
-            const location = button.getAttribute("data-location");
-
-            // Aggiungi i dettagli dell'evento alla finestra modale
-            const eventModalTitle = document.querySelector(".modal-content h2");
-            eventModalTitle.textContent = title;
-
-            // Imposta il click sul pulsante "Salva sul Calendario"
-            saveToCalendarButton.addEventListener("click", function() {
-                const eventDetails = `${title}\nData: ${date}\nOra: ${time}\nLuogo: ${location}`;
-                const calendarURL = `data:text/calendar;charset=utf-8,${encodeURIComponent(generateICS(eventDetails))}`;
-                window.open(calendarURL);
-            });
         });
     });
 
@@ -36,19 +19,25 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Funzione per generare il file ICS (iCalendar)
-    function generateICS(eventDetails) {
-        // Formato ICS
-        return `
-BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-SUMMARY:${eventDetails}
-DTSTART:${date}T${time}
-DTEND:${date}T${time}
-LOCATION:${location}
-END:VEVENT
-END:VCALENDAR
-        `;
-    }
+    const saveToCalendarButton = document.querySelector(".save-to-calendar-button");
+    saveToCalendarButton.addEventListener("click", function() {
+        const eventDetails = {
+            title: "Visita all'eremo di Pietraspaccata",
+            description: "Visita all'eremo",
+            location: "Bosco della Salandra",
+            start: [2023, 9, 20, 9, 30], // [anno, mese (da 0 a 11), giorno, ora, minuti]
+            end: [2023, 9, 20, 12, 0] // [anno, mese (da 0 a 11), giorno, ora, minuti]
+        };
+
+        const cal = ics();
+        cal.addEvent(
+            eventDetails.title,
+            eventDetails.description,
+            eventDetails.location,
+            eventDetails.start,
+            eventDetails.end
+        );
+
+        cal.download();
+    });
 });
