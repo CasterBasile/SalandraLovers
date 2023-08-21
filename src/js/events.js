bbf5da457c3d55294b96202244ca1319acff71c2
CasterBasile <basilecastrese@gmail.com>
2023-08-21 23:14:24 +0200

    3.1

--- src/js/events.js
+++ src/js/events.js
@@ -1,28 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const eventModal = document.getElementById("eventModal");
     const closeModal = document.querySelector(".close-modal");
-    const openModalButtons = document.querySelectorAll(".open-modal");
+    const openModalButtons = document.querySelectorAll(".banner-link");
+    const saveToCalendarButton = document.querySelector(".save-to-calendar-button");
 
     openModalButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            const modalId = button.getAttribute("data-modal"); // Ottieni l'ID dell'elemento modale dal data-modal
-            const modal = document.getElementById(modalId); // Ottieni l'elemento modale utilizzando l'ID
-            modal.style.display = "block"; // Mostra l'elemento modale
+        button.addEventListener("click", function(event) {
+            eventModal.style.display = "block";
+
+            const title = button.getAttribute("data-title");
+            const date = button.getAttribute("data-date");
+            const time = button.getAttribute("data-time");
+            const location = button.getAttribute("data-location");
+
+            // Aggiungi i dettagli dell'evento alla finestra modale
+            const eventModalTitle = document.querySelector(".modal-content h2");
+            eventModalTitle.textContent = title;
+
+            // Imposta il click sul pulsante "Salva sul Calendario"
+            saveToCalendarButton.addEventListener("click", function() {
+                const eventDetails = `${title}\nData: ${date}\nOra: ${time}\nLuogo: ${location}`;
+                const calendarURL = `data:text/calendar;charset=utf-8,${encodeURIComponent(generateICS(eventDetails))}`;
+                window.open(calendarURL);
+            });
         });
     });
 
     closeModal.addEventListener("click", function() {
-        const modals = document.querySelectorAll(".modal");
-        modals.forEach(modal => {
-            modal.style.display = "none"; // Chiudi tutti gli elementi modal quando viene cliccato il pulsante di chiusura
-        });
+        eventModal.style.display = "none";
     });
 
     window.addEventListener("click", function(event) {
-        const modals = document.querySelectorAll(".modal");
-        modals.forEach(modal => {
-            if (event.target === modal) {
-                modal.style.display = "none"; // Chiudi l'elemento modale quando si fa clic fuori dalla finestra modale
-            }
-        });
+        if (event.target === eventModal) {
+            eventModal.style.display = "none";
+        }
     });
+
+    // Funzione per generare il file ICS (iCalendar)
+    function generateICS(eventDetails) {
+        // Formato ICS
+        return `
+BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${eventDetails}
+DTSTART:${date}T${time}
+DTEND:${date}T${time}
+LOCATION:${location}
+END:VEVENT
+END:VCALENDAR
+        `;
+    }
 });
