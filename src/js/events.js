4467d864188105de6d3cc60229c15c95429b65c6
CasterBasile <basilecastrese@gmail.com>
2023-08-23 12:58:23 +0200

    3.5.5

--- src/js/events.js
+++ src/js/events.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const eventModal = document.getElementById("eventModal");
     const closeModal = document.querySelector(".close-modal");
-    const openModalButtons = document.querySelectorAll(".banner-link.open-modal");
+    const bannerContainer = document.querySelector(".banner-container");
 
-    openModalButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            eventModal.style.display = "block";
-        });
-    });
+    // Recupera i dati dei banner da Firebase usando fetch
+    fetch("https://salandra-lovers-default-rtdb.firebaseio.com/events.json")
+      .then(response => response.json())
+      .then(bannersData => {
+        for (const eventId in bannersData) {
+          const bannerData = bannersData[eventId];
+          createBannerElement(bannerData);
+        }
+      })
+      .catch(error => {
+        console.error('Errore durante il recupero dei dati da Firebase:', error);
+      });
 
     closeModal.addEventListener("click", function() {
         eventModal.style.display = "none";
@@ -18,19 +25,24 @@ document.addEventListener("DOMContentLoaded", function() {
             eventModal.style.display = "none";
         }
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const openLinkButton = document.querySelector(".open-link-button");
-    
-    openLinkButton.addEventListener("click", function() {
-        // Inserisci qui l'URL del link a cui vuoi indirizzare
-        const linkUrl = "https://www.facebook.com/groups/950471052240086/events";
-        
-        // Apri il link nella stessa finestra/ scheda
-        window.location.href = linkUrl;
-        
-        // Se preferisci aprire il link in una nuova finestra/ scheda, usa la seguente riga:
-        // window.open(linkUrl, "_blank");
-    });
+    // Funzione per creare elementi banner
+    function createBannerElement(bannerData) {
+        const bannerElement = document.createElement('div');
+        bannerElement.className = 'banner-container'; // <-- Cambiato il nome della classe
+        bannerElement.innerHTML = `
+            <div class="banner-link open-modal" data-modal="eventModal">
+                <div class="banner-overlay"></div>
+                <img src="${bannerData.image}" alt="${bannerData.title}">
+                <div class="banner-title">${bannerData.title}</div>
+                <div class="banner-subtitle">${bannerData.date} - ${bannerData.time}</div>
+            </div>
+        `;
+        bannerContainer.appendChild(bannerElement);
+
+        const bannerLink = bannerElement.querySelector(".banner-link");
+        bannerLink.addEventListener("click", function() {
+            eventModal.style.display = "block";
+        });
+    }
 });
