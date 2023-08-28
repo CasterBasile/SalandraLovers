684cc274199047823c7a9f9866dfff5ed3022929
CasterBasile <basilecastrese@gmail.com>
2023-08-28 22:59:09 +0200

    8.8

--- src/js/home.js
+++ src/js/home.js
@@ -6,6 +6,10 @@ const images = [
     '/src/images/gallery/gallery3.jpg',
     '/src/images/gallery/gallery4.jpg',
     '/src/images/gallery/gallery5.jpg',
+    '/src/images/gallery/gallery6.jpg',
+    '/src/images/gallery/gallery7.jpg',
+    '/src/images/gallery/gallery8.jpg',
+    '/src/images/gallery/gallery9.jpg',
 ];
 
 let currentIndex = 0;
