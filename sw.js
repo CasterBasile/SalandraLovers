3f1d9ca745c0e75ac5ac83a0088a7f9b1ad97237
CasterBasile <basilecastrese@gmail.com>
2023-11-05 18:33:39 +0100

    update

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v44';
-var CACHE_DYNAMIC_NAME = 'dynamic-v44';
+var CACHE_STATIC_NAME = 'static-v45';
+var CACHE_DYNAMIC_NAME = 'dynamic-v45';
 var STATIC_FILES = [
   '/',
   '/index.html',
