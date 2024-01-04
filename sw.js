3b6cc9055cce6a55797dc4ed039fdb2ba5c325d2
CasterBasile <basilecastrese@gmail.com>
2024-01-04 20:19:56 +0100

    newLogo

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v49';
-var CACHE_DYNAMIC_NAME = 'dynamic-v49';
+var CACHE_STATIC_NAME = 'static-v50';
+var CACHE_DYNAMIC_NAME = 'dynamic-v50';
 var STATIC_FILES = [
   '/',
   '/index.html',
