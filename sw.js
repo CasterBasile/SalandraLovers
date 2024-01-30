4ffa7b9f90340aec1ab51db7894dc67c0eee79b4
CasterBasile <basilecastrese@gmail.com>
2024-01-30 13:54:38 +0100

    eventJanuary

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v52';
-var CACHE_DYNAMIC_NAME = 'dynamic-v52';
+var CACHE_STATIC_NAME = 'static-v53';
+var CACHE_DYNAMIC_NAME = 'dynamic-v53';
 var STATIC_FILES = [
   '/',
   '/index.html',
