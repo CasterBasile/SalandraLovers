6b38085e50387e8e88822e9f709a227adcaee0f3
CasterBasile <basilecastrese@gmail.com>
2024-02-09 10:15:05 +0100

    popUp

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v54';
-var CACHE_DYNAMIC_NAME = 'dynamic-v54';
+var CACHE_STATIC_NAME = 'static-v55';
+var CACHE_DYNAMIC_NAME = 'dynamic-v55';
 var STATIC_FILES = [
   '/',
   '/index.html',
