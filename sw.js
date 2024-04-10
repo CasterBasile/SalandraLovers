3e432778d6a0898a1871cec2ca5aef162a0565ae
CasterBasile <basilecastrese@gmail.com>
2024-04-10 22:16:21 +0200

    recluta2.0

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v57';
-var CACHE_DYNAMIC_NAME = 'dynamic-v57';
+var CACHE_STATIC_NAME = 'static-v58';
+var CACHE_DYNAMIC_NAME = 'dynamic-v58';
 var STATIC_FILES = [
   '/',
   '/index.html',
